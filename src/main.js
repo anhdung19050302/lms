b03ebd1e26ca057262c5a88d43238ec0a4dbe2c4
Dungnguyen2732001 <dung.nguyenduc@drcomgroup.com>
2023-12-22 13:41:07 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "@/assets/style/tailwind.css";
+import "@/assets/style/global.css";
 
-createApp(App).use(router).mount("#app");
+let app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.{vue,js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
