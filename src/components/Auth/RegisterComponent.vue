131f9a4f37ec99dd610d86b49c621f0918e08244
Dungnguyen2732001 <anhdung19050302@gmail.com>
2023-12-28 07:54:45 +0700

    Login and register

--- src/components/Auth/RegisterComponent.vue
+++ src/components/Auth/RegisterComponent.vue
@@ -14,7 +14,7 @@
           class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
         >
           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-            Sign in to our platform
+            Đăng ký để học
           </h3>
           <button
             type="button"
